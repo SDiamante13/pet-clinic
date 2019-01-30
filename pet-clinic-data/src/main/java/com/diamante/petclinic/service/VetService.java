69ec2f113a45cc5ec58e3a0cc9f46b776ac80fb7
Steven <stevendiamante13@gmail.com>
2019-01-29 21:43:02 -0600

    Add base class CrudService for a common parent class for services.

--- pet-clinic-data/src/main/java/com/diamante/petclinic/service/VetService.java
+++ pet-clinic-data/src/main/java/com/diamante/petclinic/service/VetService.java
@@ -2,13 +2,5 @@ package com.diamante.petclinic.service;
 
 import com.diamante.petclinic.entity.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
