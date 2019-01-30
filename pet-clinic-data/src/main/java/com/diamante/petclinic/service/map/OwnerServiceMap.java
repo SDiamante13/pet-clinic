5b3dfd2b7a9c947e4407fa223776b3f5cbb5fc95
Steven <stevendiamante13@gmail.com>
2019-01-29 22:04:05 -0600

    Add map service implementations.

---
+++ pet-clinic-data/src/main/java/com/diamante/petclinic/service/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package com.diamante.petclinic.service.map;
+
+import com.diamante.petclinic.entity.Owner;
+import com.diamante.petclinic.service.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap  extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(), owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+}
